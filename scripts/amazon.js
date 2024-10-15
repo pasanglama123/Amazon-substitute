9f4169f7f2b9ab0782a8e99b6cf70f3685f6e77d
pasang tengin lama <pasang201722@gamil.com>
2024-10-15 13:13:03 +0545

    cart-quantity update

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -55,7 +55,6 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
 document.querySelectorAll('.js-add-to-cart')
  .forEach((button)=> {
     button.addEventListener('click', ()=>{
@@ -77,6 +76,16 @@ document.querySelectorAll('.js-add-to-cart')
                 quantity:1
             });
         }
+
+        let cartQuantity = 0;
+        cart.forEach((item)=>{
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+
+        console.log(cartQuantity);
         console.log(cart);
 
     });
