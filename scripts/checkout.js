72df7f95d0fd985141f283235d48e131e261bd55
pasang tengin lama <pasang201722@gamil.com>
2024-10-24 22:14:56 +0545

    update cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummeryHTML = '';
 cart.forEach((cartItem) => {
