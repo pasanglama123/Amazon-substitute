f81bcda00a568b25d242a538f21b9d4f343a3e62
pasang tengin lama <pasang201722@gamil.com>
2025-01-02 12:00:19 +0545

    convert products into the classes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -63,7 +63,7 @@ export function renderOrderSummary() {
                   ${matchingItem.name}
               </div>
               <div class="product-price">
-                  $${formatCurrency(matchingItem.priceCents)}
+                ${matchingProduct.getPrice();}
               </div>
               <div class="product-quantity">
                   <span>
